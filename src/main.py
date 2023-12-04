a22c11a36344877c676601694fc4a111974211bd
olegggwp <boolvargosha@yandex.ru>
2023-12-05 00:28:26 +0300

    added /add handler

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import uuid
 from loguru import logger
 from messages import messages
 from model import do_query, classify_face
+from pathlib import Path
+import json
 
 bot = telebot.TeleBot(os.environ["BOT_TOKEN"])
 
@@ -43,6 +45,73 @@ def photo(message):
         
         for entry in results:
             bot.send_message(message.chat.id, f"{entry['text']}\n\n - {entry['author']}, «{entry['piece']}»")
+
+
+json_file_path = "user_quotes.json"
+
+@bot.message_handler(commands=['/add'])
+def add_quote_handler(message):
+    user_id = message.from_user.id
+    bot.send_message(user_id, "введите цитату:")
+    bot.register_next_step_handler(message, save_quote_step)
+
+def save_quote_step(message):
+    user_id = message.from_user.id
+    quote_text = message.text
+    bot.send_message(user_id, "Теперь автора цитаты:")
+    bot.register_next_step_handler(message, save_author, quote_text)
+
+def save_author(message, quote_text):
+    user_id = message.from_user.id
+
+    author_text = message.text
+    bot.send_message(user_id, "Откуда цитата:")
+    bot.register_next_step_handler(message, save_work, quote_text, author_text)
+
+
+def get_user_quotes(user_id):
+    if Path(json_file_path).is_file():
+        with open(json_file_path, "r") as file:
+            all_quotes = json.load(file)
+    if str(user_id) not in all_quotes:
+        user_quotes = {"quotes": []}
+    else:
+        user_quotes = all_quotes[str(user_id)]
+    return user_quotes
+
+def set_user_quotes(user_quotes, user_id):
+    with open(json_file_path, "r") as file:
+        all_quotes = json.load(file)
+    all_quotes[str(user_id)] = user_quotes
+    with open(json_file_path, "w") as file:
+        json.dump(all_quotes, file, indent=4)
+
+def save_quote(user_id, quote_text, author_text, work_text):
+    user_quotes = get_user_quotes(user_id)
+
+    user_quotes["quotes"].append({
+        "quote": quote_text,
+        "author": author_text,
+        "work": work_text
+    })
+
+    set_user_quotes(user_quotes, user_id)
+
+def save_work(message, quote_text, author_text):
+    user_id = message.from_user.id
+    work_text = message.text
+    save_quote(user_id, quote_text, author_text, work_text)
+    bot.send_message(user_id, "Quote added successfully!")
+
+@bot.message_handler(commands=['view'])
+def view_quotes(message):
+    user_id = message.from_user.id
+    user_quotes = get_user_quotes(user_id)
+    if "quotes" in user_quotes and user_quotes["quotes"]:
+        quotes_text = "\n\n".join([f"\"{quote['quote']}\" - {quote['author']}, {quote['work']}" for quote in user_quotes["quotes"]])
+        bot.send_message(user_id, f"Your quotes:\n{quotes_text}")
+    else:
+        bot.send_message(user_id, "You haven't added any quotes yet.")
 
 
 if __name__ == "__main__":
